5f90e989b7519b281ab4b8c0382b40f6a9d3cba7
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-06-05 14:51:36 -0400

    add placeholder tours

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
 import toursData from '@/public/tours/tours.json';
-import { ArtworkNarration, GalleryTour } from '@/types';
-import { BkmLogo } from '@/components/ui/bkm-logo';
+import { GalleryTour } from '@/types';
 
-import { buttonVariants } from '@/components/ui/button';
+import { BkmLogo } from '@/components/ui/bkm-logo';
+import { Button } from '@/components/ui/button';
 
 export async function generateMetadata({ params }): Promise<Metadata> {
   return {
@@ -30,22 +31,22 @@ export default function Page() {
       </div>
       <div className="flex flex-col gap-4">
         {tours.map((tour) => (
-          <div className="rounded-lg p-4 hover:bg-neutral-800">
+          <div
+            className="rounded-lg p-4 hover:bg-neutral-800"
+          >
             <Link key={tour.slug} href={`/tour/${tour.slug}`}>
-              <h2 className="text-xl font-extrabold">{tour.name}</h2>
+              <Image
+                className="h-48 w-full object-cover"
+                alt="Artist"
+                src={`/tours/${tour?.slug}/image.jpg`}
+                width="500"
+                height="500"
+              />
+              <h2 className="text-xl font-extrabold mt-2">{tour.name}</h2>
               <p className="text-neutral-300">{tour.description}</p>
             </Link>
           </div>
         ))}
-      </div>
-
-      <div className="flex gap-4">
-        <Link
-          href="/tour/highlights/storm_in_the_rocky_mountains_mt_rosalie_albert_bierstadt"
-          className={buttonVariants({ size: 'lg' })}
-        >
-          Highlights Tour
-        </Link>
       </div>
     </section>
   );
