daf7741b307397b7480c0e4c2b323456939e18fd
c-facade <francecipinna@gmail.com>
2025-06-21 13:23:20 +0200

    prima di cambiare la struttura del sito

--- app/page.tsx
+++ app/page.tsx
@@ -23,13 +23,15 @@ export default function Page() {
         <p className="max-w-[700px] text-lg text-neutral-700 dark:text-neutral-400 sm:text-xl">
 				Ascolta le audioguide dei seguenti musei e mostre.
 				</p>
-      </div>
+			</div>
+			<button>Cliccami</button>
+			
       <div className="flex flex-col gap-4">
         {tours.map((tour) => (
           <div
             className="rounded-lg p-4 hover:bg-neutral-800" key={tour.slug}
           >
-            <Link key={tour.slug} href={`/tour/${tour.slug}`}>
+						<Link key={tour.slug} href={`/tour/${tour.slug}/intro`}>
               <Image
                 className="h-48 w-full object-cover"
                 alt="Artist"
@@ -39,7 +41,7 @@ export default function Page() {
               />
               <h2 className="text-xl font-extrabold mt-2">{tour.name}</h2>
               <p className="text-neutral-300">{tour.description}</p>
-            </Link>
+						</Link>
           </div>
         ))}
       </div>
