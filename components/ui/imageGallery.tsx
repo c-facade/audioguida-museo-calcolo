8d7316f907a29685b600c5f4a85e5f0a4cb9ab0e
c-facade <francecipinna@gmail.com>
2025-07-04 19:44:01 +0200

    tentativo immagini aggiuntive

--- components/ui/imageGallery.tsx
+++ components/ui/imageGallery.tsx
@@ -1,10 +1,6 @@
-'use client'
 import Image from 'next/image';
-import LightBox from "yet-another-react-lightbox";
-import { useState } from "react";
 
 export default function ImageGallery( {galleryTour, artwork}) {
-	const [open, setOpen] = useState(true);
 	if(!artwork.images){
 		return null;
 	}
@@ -12,24 +8,13 @@ export default function ImageGallery( {galleryTour, artwork}) {
 	for(let i = 1; i<artwork.images+1; i++){
 		array.push(`/tours/${galleryTour.slug}/${artwork.slug}/image${i}.jpg`);
 	}
-	const slides = array.map((filename) => ({
-		src: filename,
-		width: 200,
-		height: 200,
-	}));
 
 	return (
-	<div>
+		<div className="mt-4">
 	{ array.map((filename) =>
-							<Image key={filename} src={filename} alt="decorativa" height={200} width={200} onClick={() => setOpen(true)} />
+							<Image className="m-1 inline" key={filename} src={filename} alt="decorativa" height={200} width={200}/>
 						 )
 	}
-	<LightBox
-		slides ={slides}
-		open={open}
-		close={() => setOpen(false)}
-		/>
-	Raga che succede
 	</div>
 	);
 }
