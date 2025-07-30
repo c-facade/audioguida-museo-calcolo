54a6c6cd1506e94a1fc8a802aa2e6601df273b81
c-facade <francecipinna@gmail.com>
2025-07-30 15:08:57 +0200

    immagini-supporto-guida

---
+++ app/supporto-guida/page.tsx
@@ -0,0 +1,11 @@
+import SupportGallery from "@/components/ui/supportGallery";
+
+export default function Page(){
+
+	return (
+		<div>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/bC8vuHh3hfs?si=QrwGwNEstAR_qZ2W" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+			< SupportGallery/>
+		</div>
+			);
+}
