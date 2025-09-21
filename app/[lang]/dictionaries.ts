e38b0b64cb805ccd7ef9020d8d6d38eb9f7c74ce
c-facade <francecipinna@gmail.com>
2025-09-21 15:53:47 +0200

    updated dictionaries.ts

--- app/[lang]/dictionaries.ts
+++ app/[lang]/dictionaries.ts
@@ -3,7 +3,15 @@ import 'server-only'
 const dictionaries = {
 	en: () => import('../../dict/en.json').then((module) => module.default),
 	it: () => import('../../dict/it.json').then((module) => module.default),
-	fr: () => import('../../dict/fr.json').then((module) => module.default)
+	fr: () => import('../../dict/fr.json').then((module) => module.default),
+}
+
+const toursData = {
+	en: () => import('../../public/tours/tours_en.json').then((module) => module.default),
+	it: () => import('../../public/tours/tours_it.json').then((module) => module.default),
+	fr: () => import('../../public/tours/tours_fr.json').then((module) => module.default),
 }
 
 export const getDictionary = async (locale : string) => dictionaries[locale]()
+export const getToursData = async (locale: string) => toursData[locale]()
+
