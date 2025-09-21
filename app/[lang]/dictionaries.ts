f7973c2f8b00643a4b2b1d1efcb92009cfe81891
c-facade <francecipinna@gmail.com>
2025-09-21 15:50:21 +0200

    dictionaries + french

--- app/[lang]/dictionaries.ts
+++ app/[lang]/dictionaries.ts
@@ -3,6 +3,7 @@ import 'server-only'
 const dictionaries = {
 	en: () => import('../../dict/en.json').then((module) => module.default),
 	it: () => import('../../dict/it.json').then((module) => module.default),
+	fr: () => import('../../dict/fr.json').then((module) => module.default)
 }
 
 export const getDictionary = async (locale : string) => dictionaries[locale]()
