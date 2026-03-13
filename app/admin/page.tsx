eed830d4f982176b4781f7e1ec2cc1afe96aea62
c-facade <francecipinna@gmail.com>
2026-03-13 14:44:14 +0100

    install tremor, change admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,15 +2,21 @@
 import { prisma } from "@/lib/prisma";
 
 export default async function AdminPage() {
-  const visits = await prisma.visit.count();
-	
+	// just a count of total visits in database
+	const visits = await prisma.visit.count();
+	// how many new and recurring visitors for each day
+	const dailyStats = await prisma.visit.groupBy({
+		by: ['day', 'isNew'],
+		_count: {id: true},
+		orderBy: { day : 'asc'}
+	});
+
+	// adapting it to fit for tremor
   return (
     <div>
       <h1 className="mt-2 text-xl font-extrabold" >Admin Analytics</h1>
-			<p>Total visitors: <pre>{JSON.stringify(visits, null, 2)}</pre>
-				</p>
-			<p>Total visitors: <pre>{JSON.stringify(visits, null, 2)}</pre>
-				</p>
-    </div>
+			<p>Total visitors:</p> <pre>{JSON.stringify(visits, null, 2)}</pre>
+			<p>Dailystats: </p> <pre>{JSON.stringify(dailyStats, null, 2)}</pre>
+		   </div>
   );
 }
