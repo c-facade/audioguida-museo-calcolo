de25a893aa67783555700230cba1932f7a9853bd
c-facade <francecipinna@gmail.com>
2026-03-13 13:56:24 +0100

    day-to-day

---
+++ prisma/migrations/20260313125540_day_to_day/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `Day` on the `Visit` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Visit" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "path" TEXT NOT NULL,
+    "language" TEXT,
+    "browserLang" TEXT,
+    "isNew" BOOLEAN NOT NULL,
+    "referrer" TEXT,
+    "userAgent" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "day" TEXT
+);
+INSERT INTO "new_Visit" ("browserLang", "createdAt", "id", "isNew", "language", "path", "referrer", "userAgent") SELECT "browserLang", "createdAt", "id", "isNew", "language", "path", "referrer", "userAgent" FROM "Visit";
+DROP TABLE "Visit";
+ALTER TABLE "new_Visit" RENAME TO "Visit";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
